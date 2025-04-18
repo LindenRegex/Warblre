be6f091210a52975446fa314ce5de0ac786ed362
Noe De Santo <noe@ef5.ch>
2025-04-18 13:53:36 -0400

    Add `SpecMerger` to flake

--- flake.nix
+++ flake.nix
@@ -5,6 +5,11 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    spec-merger = {
+      url = "github:Ef55/SpecMerger/38ac474cca1788ec4fb4d85ecaaa8c81aecf41f6?dir=.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
     melange = {
       url = "github:melange-re/melange/v3-414";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -12,7 +17,7 @@
     };
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, flake-utils, melange }@input: 
+  outputs = { self, nixpkgs, nixpkgs-unstable, spec-merger, flake-utils, melange }@input:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; overlays = [ melange.overlays.default ]; };
@@ -36,6 +41,7 @@
 
                 coqPackages.serapi
                 python311Packages.alectryon
+                spec-merger.packages.${system}.spec-merger
 
                 nodejs_21
                 nodePackages.webpack-cli
