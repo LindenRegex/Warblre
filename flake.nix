5affa13fd7e686e4864265dfbc9f910d61a7b736
Noe De Santo <noe@ef5.ch>
2025-11-25 22:00:30 -0500

    Update flake to Rocq (9.1)

--- flake.nix
+++ flake.nix
@@ -2,26 +2,19 @@
   description = "A mechanization of the specification of ECMAScript regexes.";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     spec-merger = {
       url = "github:Ef55/SpecMerger/38ac474cca1788ec4fb4d85ecaaa8c81aecf41f6?dir=.nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
-    };
-    melange = {
-      url = "github:melange-re/melange/v3-414";
-      inputs.nixpkgs.follows = "nixpkgs";
+      # inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-utils.follows = "flake-utils";
     };
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, spec-merger, flake-utils, melange }@input:
+  outputs = { self, nixpkgs, spec-merger, flake-utils }@input:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; overlays = [ melange.overlays.default ]; };
-        pkgs-unstable = import nixpkgs-unstable { inherit system; };
+        pkgs = nixpkgs.legacyPackages.${system};
 
         spec-diff = pkgs.writeShellApplication {
           name = "spec-diff";
@@ -37,10 +30,11 @@
         devShells = {
             default = pkgs.mkShell {
               buildInputs = with pkgs; [
-                coq
+                coq_9_1
+                coqPackages_9_1.stdlib
 
                 ocaml
-                pkgs-unstable.dune_3
+                dune_3
                 ocamlPackages.ocamlformat
                 ocamlPackages.ocaml-lsp
                 ocamlPackages.findlib
@@ -50,12 +44,12 @@
                 ocamlPackages.melange
                 ocamlPackages.zarith
 
-                coqPackages.serapi
-                python311Packages.alectryon
+                # coqPackages.serapi
+                # python311Packages.alectryon
                 spec-merger.packages.${system}.spec-merger
                 spec-diff
 
-                nodejs_21
+                nodejs_24
                 nodePackages.webpack-cli
               ];
           };
